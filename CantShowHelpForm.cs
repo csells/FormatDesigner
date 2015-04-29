5bc64c6411e9941e8881c40f1dc5cb896eae1cfa
Atif Aziz <code@raboof.com>
2015-04-29 18:26:37 +0200

    Split designer code in forms via partials

--- CantShowHelpForm.cs
+++ CantShowHelpForm.cs
@@ -20,27 +20,15 @@ THE SOFTWARE.
 */
 #endregion
 
-using System;
-using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
-using System.Windows.Forms;
-
 namespace FormatDesigner
 {
-	/// <summary>
+    using System.Windows.Forms;
+
+    /// <summary>
 	/// Summary description for CantShowHelpForm.
 	/// </summary>
-	public class CantShowHelpForm : System.Windows.Forms.Form
+	public partial class CantShowHelpForm : Form
 	{
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.TextBox textBox1;
-    private System.Windows.Forms.Button okButton;
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.Container components = null;
-
 		public CantShowHelpForm()
 		{
 			//
@@ -52,77 +40,5 @@ namespace FormatDesigner
 			// TODO: Add any constructor code after InitializeComponent call
 			//
 		}
-
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
-
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-      this.label1 = new System.Windows.Forms.Label();
-      this.textBox1 = new System.Windows.Forms.TextBox();
-      this.okButton = new System.Windows.Forms.Button();
-      this.SuspendLayout();
-      // 
-      // label1
-      // 
-      this.label1.Location = new System.Drawing.Point(8, 16);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(336, 56);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "This application doesn\'t have the permission to show the web page for the Format " +
-        "Specifiers appendix from C# in a Nutshell by Drayton, et al. You can launch it y" +
-        "ourself using the following URL:";
-      // 
-      // textBox1
-      // 
-      this.textBox1.Location = new System.Drawing.Point(16, 88);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.ReadOnly = true;
-      this.textBox1.Size = new System.Drawing.Size(320, 20);
-      this.textBox1.TabIndex = 1;
-      this.textBox1.Text = "http://www.oreilly.com/catalog/csharpnut/chapter/appb.pdf";
-      // 
-      // okButton
-      // 
-      this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.okButton.Location = new System.Drawing.Point(140, 120);
-      this.okButton.Name = "okButton";
-      this.okButton.TabIndex = 2;
-      this.okButton.Text = "OK";
-      // 
-      // CantShowHelpForm
-      // 
-      this.AcceptButton = this.okButton;
-      this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-      this.ClientSize = new System.Drawing.Size(354, 152);
-      this.ControlBox = false;
-      this.Controls.Add(this.okButton);
-      this.Controls.Add(this.textBox1);
-      this.Controls.Add(this.label1);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-      this.Name = "CantShowHelpForm";
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-      this.Text = "Can\'t Show Help";
-      this.ResumeLayout(false);
-
-    }
-		#endregion
 	}
 }
